3f96b0090ae3c578ae1a47027727620d0c35019a
dbxgamer <50654106+dbxgamer@users.noreply.github.com>
2019-06-24 16:46:33 +0530

    Update afk.py

--- tg_bot/modules/afk.py
+++ tg_bot/modules/afk.py
@@ -22,7 +22,7 @@ def afk(bot: Bot, update: Update):
         reason = ""
 
     sql.set_afk(update.effective_user.id, reason)
-    update.effective_message.reply_text("{} is now AFK!".format(update.effective_user.first_name))
+    update.effective_message.reply_text("{} is now AFK! He won't play for some time".format(update.effective_user.first_name))
 
 
 @run_async
@@ -34,7 +34,7 @@ def no_longer_afk(bot: Bot, update: Update):
 
     res = sql.rm_afk(user.id)
     if res:
-        update.effective_message.reply_text("{} is no longer AFK!".format(update.effective_user.first_name))
+        update.effective_message.reply_text("{} is no longer AFK! Enjoy Gaming!".format(update.effective_user.first_name))
 
 
 @run_async
@@ -64,7 +64,7 @@ def reply_afk(bot: Bot, update: Update):
                     if not reason:
                         res = "{} is AFK!".format(fst_name)
                     else:
-                        res = "{} is AFK! says its because of:\n{}".format(fst_name, reason)
+                        res = "{} is AFK! He won,t play because of:\n{}".format(fst_name, reason)
                     message.reply_text(res)
 
 
@@ -76,7 +76,7 @@ __help__ = """
  - /afk <reason>: mark yourself as AFK.
  - brb <reason>: same as the afk command - but not a command.
 
-When marked as AFK, any mentions will be replied to with a message to say you're not available!
+Real gamers know what is AFK!
 """
 
 __mod_name__ = "AFK"
